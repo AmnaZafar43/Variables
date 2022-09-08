3677391833aab408570d02757f2abaef83b7d389
Amna Zafar <Amnazafar052@gmail.com>
2022-09-08 13:50:34 -0700

    first commit

---
+++ task.py
@@ -0,0 +1,12 @@
+a = 10
+b = 20.6
+c = "aymee"
+
+print(a+b)
+atype = type(a)
+print(atype)
+
+'''
+   We can create variables like this but create variable according to rules of variable
+   here we can also find type of variable by writing type(variable name).
+'''
